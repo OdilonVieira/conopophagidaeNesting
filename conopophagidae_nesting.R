6acec09ab5d077042b91f850751ac2e465379164
Odilon Vieira <35323414+OdilonVieira@users.noreply.github.com>
2023-02-08 11:24:22 -0300

    add chart plot and other changes

--- conopophagidae_nesting.R
+++ conopophagidae_nesting.R
@@ -4,6 +4,7 @@
 #libraries required
 library(dplyr)
 library(data.table)
+library(ggplot2)
 
 #measurements datasets
 conopophagidaeNest = fread("nest_conopophagidae.csv",check.names = T)
@@ -78,3 +79,37 @@ summarizedClutches_txt = group_by(totalClutches, Species) %>%
 nestingConopophagidaeStatistics = left_join(summarizedNests_txt,summarizedEggs_txt,by="Species") %>%
   left_join(summarizedClutches_txt,by="Species") %>%
   left_join(sources,by="Species")
+
+#Specimens of Conopophaga cearae on MHNCE and MN collections with information about gonads
+gonads = tibble(
+  Voucher = c("MHNCE-0200","MHNCE-0201","MHNCE-0341",rep("MN-42745",2),rep("MN-42746",2),
+              "MN-34554",rep("MN-34555",2),rep("MN-35001",2),"MN-36378","MN-36938",
+              rep("MN-43276",2),"MN-43309"),
+  width = c(2.8, 2.5, 5, 5.69, 5.08, 3.83, 3.43, 3.25, 1.68, 2.73, 3.05, 3.47, 3.63,
+            3.52, 2.47, 2.66, 3.47),
+  length = c(3, 7, 6.5, 8.70, 9.17, 8.18, 8.14, 4.6, 2.85, 4.13, 4.63, 5.43, 5,
+             3.92, 4, 4, 3.88),
+  sex = c(rep("Male",7), "Female",rep("Male",4),rep("Female",2),rep("Male",2),"Female"),
+  monthNumber = c(10, 12, 1, rep(12,4), rep(2,5), 4, 2, rep(5,2), 7),
+  monthText = c("Oct.", "Dec.", "Jan.", rep("Dec.",4), rep("Feb.",5), "Apr.", "Feb.", rep("May",2), "Jul.")
+)
+
+monthOrder = c("Aug.","Sep.","Oct.","Nov.",
+               "Dec.","Jan.","Feb.","Mar.",
+               "Apr.","May","Jun.","Jul.")
+breeding = c("Dec.","Jan.","Feb.","Mar.")
+#Plot the gonad 
+ggplot(gonads) +
+  aes(x = factor(monthText, levels = monthOrder), shape=sex) +
+  geom_point(aes(y = width), colour="red", size=6, show.legend = F) +
+  geom_point(aes(y = length), colour="black", size=6, show.legend = F) +
+  xlab("Month") +
+  ylab("Gonad size (mm)") +
+  scale_x_discrete(breaks=monthOrder,drop=F) +
+  scale_y_continuous(breaks = 0:10) +
+  scale_shape_manual(values=c("Female"=17,"Male"=19)) +
+  theme_bw() +
+  theme(text = element_text(color="black", size=20),
+    axis.text = element_text(color="black", size=20),
+    axis.text.x = element_text(face=ifelse(monthOrder %in% breeding,"bold","plain")),
+    panel.border = element_rect(colour="black", linewidth = 1))
